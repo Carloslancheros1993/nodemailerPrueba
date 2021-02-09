e1ce398d1812a2e5bb7dc741af4790a4b743bbc1
Carlos <u2204240@unimilitar.edu.co>
2021-02-09 12:33:41 -0500

    nodemailer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import authRouter from "./routes/auth";
+import sendEmail from "./utils/nodemailer";
 
 const app = express();
 
 app.use(express.json());
 app.use(authRouter);
+
+app.post("/send-email", (req, res) => {
+    try{
+        sendEmail();
+        res.json({
+            message: "El correo ha sido enviado satisfactoriamente"
+        });
+    }catch(error){
+        console.log(error);
+    }
+});
 
 export default app;
--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,11 +1,27 @@
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
+
 
 //Completar la funcion para generar un token JWT en base al usuario que ha iniciado sesion
 export const generateJWT = (user) => {
-
+    const userObj = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.emal
+    }
+    const token = jwt.sign(userObj, process.env.SECRET_KEY, {algorithm: "HS384", expiresIn: "1h"});
+    return token;
 }
 
 //Validar el token 
-const validateJWT = (req, res) => {
-    
-}
+export const validateJWT = async (req, res, next) => {
+    const token = req.headers['x-access-token'];
+    if (!token) {
+        return res.status(401).send({ auth: false, message: 'No token provided' });
+    }
+    // Decode the Tokenreq.userId = decoded.id;
+    const decoded = await jwt.verify(token, config.secret);
+    req.userId = decoded.id;
+    next();
+    }
+
